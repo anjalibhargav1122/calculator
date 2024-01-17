c4ae9a51ab727d173ce99f7d735542b5d662c4d3
Anjali Bhargav <155630486+anjalibhargav1122@users.noreply.github.com>
2024-01-17 15:18:43 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,40 @@
+let str = " ";
+
+let buttons = document.querySelectorAll("button");
+
+Array.from(buttons).forEach((button) => {
+
+    button.addEventListener("click", (e) => {
+
+        if (e.target.innerHTML == "=") {
+
+            str = eval(str);
+            document.querySelector("input").value = str;
+
+        }
+
+        else if (e.target.innerHTML == "C") {
+
+            str = " ";
+            document.querySelector("input").value = str;
+
+        }
+
+        else if (e.target.innerHTML == "Del") {
+
+            str = str.substr(0 , str.length-1);
+            document.querySelector("input").value = str;
+
+        }
+
+        else {
+            console.log(e.target)
+            str = str + e.target.innerHTML;
+            document.querySelector("input").value = str;
+        }
+
+    }
+    )
+
+}
+)
